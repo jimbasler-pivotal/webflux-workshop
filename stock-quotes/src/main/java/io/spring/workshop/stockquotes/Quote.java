155eabaf1f23e6b509d7d9b0f9354d9ed5ee3133
Jim Basler <jbasler@pivotal.io>
2019-07-09 12:45:47 -0500

    Add Lombok

--- stock-quotes/src/main/java/io/spring/workshop/stockquotes/Quote.java
+++ stock-quotes/src/main/java/io/spring/workshop/stockquotes/Quote.java
@@ -1,9 +1,14 @@
 package io.spring.workshop.stockquotes;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.math.BigDecimal;
 import java.math.MathContext;
 import java.time.Instant;
 
+@Getter @Setter @NoArgsConstructor
 public class Quote {
 
 	private static final MathContext MATH_CONTEXT = new MathContext(2);
@@ -14,9 +19,6 @@ public class Quote {
 
 	private Instant instant;
 
-	public Quote() {
-	}
-
 	public Quote(String ticker, BigDecimal price) {
 		this.ticker = ticker;
 		this.price = price;
@@ -24,30 +26,6 @@ public class Quote {
 
 	public Quote(String ticker, Double price) {
 		this(ticker, new BigDecimal(price, MATH_CONTEXT));
-	}
-
-	public String getTicker() {
-		return ticker;
-	}
-
-	public void setTicker(String ticker) {
-		this.ticker = ticker;
-	}
-
-	public BigDecimal getPrice() {
-		return price;
-	}
-
-	public void setPrice(BigDecimal price) {
-		this.price = price;
-	}
-
-	public Instant getInstant() {
-		return instant;
-	}
-
-	public void setInstant(Instant instant) {
-		this.instant = instant;
 	}
 
 	@Override
--- trading-service/src/main/java/io/spring/workshop/tradingservice/TradingUser.java
+++ trading-service/src/main/java/io/spring/workshop/tradingservice/TradingUser.java
@@ -1,9 +1,13 @@
 package io.spring.workshop.tradingservice;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
+@Getter @Setter @NoArgsConstructor
 public class TradingUser {
 
 	@Id
@@ -13,9 +17,6 @@ public class TradingUser {
 
 	private String fullName;
 
-	public TradingUser() {
-	}
-
 	public TradingUser(String id, String userName, String fullName) {
 		this.id = id;
 		this.userName = userName;
@@ -24,30 +25,6 @@ public class TradingUser {
 
 	public TradingUser(String userName, String fullName) {
 		this.userName = userName;
-		this.fullName = fullName;
-	}
-
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	public String getUserName() {
-		return userName;
-	}
-
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-
-	public String getFullName() {
-		return fullName;
-	}
-
-	public void setFullName(String fullName) {
 		this.fullName = fullName;
 	}
 
